cba95cc65ce651db7f171795fc1d15aa3298fbfa
Oleksandr Brylov <oleksandr.brylov@advancedmd.com>
2020-12-11 13:17:03 +0200

    Navigation improved

--- ReceipeBookApp/src/app/app.component.ts
+++ ReceipeBookApp/src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
+  loadedFeature = 'recipe';
+  navigate(feature: string) {
+    this.loadedFeature = feature;
+  }
 }
